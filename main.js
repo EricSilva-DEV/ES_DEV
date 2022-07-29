340d44dc02e952a576aee5af5efe6bd279e6e1ad
EricSilva-DEV <ericsilva.ux@gmail.com>
2022-07-29 14:55:41 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+/*=== Abrir e fechar menu ====*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+/*==== Acção de clicar e fechar menu ======*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
